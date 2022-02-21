16c620b359be51abc12e599df683fac3d6322dbe
Sam Bencivengo <sambencivengo@gmail.com>
2022-02-20 17:07:50 -0800

    added id numbers to the data, still unsure whether to keep it on the frontend or the backend:

--- client/src/App.js
+++ client/src/App.js
@@ -8,50 +8,62 @@ function App() {
 	const [roll, setRoll] = useState(null);
 	const data = [
 		{
+			id: 1,
 			language: 'Italian',
 			word: 'Salute',
 		},
 		{
+			id: 2,
 			language: 'Japanese',
 			word: '乾杯 (Kanpai) ',
 		},
 		{
+			id: 3,
 			language: 'Irish',
 			word: 'Sláinte (slawn-cha)',
 		},
 		{
+			id: 4,
 			language: 'German',
 			word: 'Prost',
 		},
 		{
+			id: 5,
 			language: 'French',
 			word: 'Santé',
 		},
 		{
+			id: 6,
 			language: 'Danish',
 			word: 'Skål',
 		},
 		{
+			id: 7,
 			language: 'Dutch',
 			word: 'Proost',
 		},
 		{
+			id: 8,
 			language: 'English',
 			word: 'Cheers',
 		},
 		{
+			id: 9,
 			language: 'Greek',
 			word: 'Στην υγειά σας (Stin Eye-ee-yass-ooh)',
 		},
 		{
+			id: 10,
 			language: 'Hawaiin',
 			word: 'Kāmau',
 		},
 		{
+			id: 11,
 			language: 'Hebrew',
 			word: "L'chaim",
 		},
 		{
+			id: 12,
 			language: 'Mandarin',
 			word: '干杯 (gān bēi)',
 		},
