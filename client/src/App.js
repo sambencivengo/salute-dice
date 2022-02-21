31080ca4c720bb90f5426e18612f3d0e6f561648
Sam Bencivengo <sambencivengo@gmail.com>
2022-02-20 17:05:21 -0800

    set up barebones dice roll that generates a random number based on the length of the data

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import NumberOfRolls from './components/NumberOfRolls';
+import React, { useState } from 'react';
+import { Button, Text } from '@chakra-ui/react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [numRolls, setNumRolls] = useState(3);
+	const [roll, setRoll] = useState(null);
+	const data = [
+		{
+			language: 'Italian',
+			word: 'Salute',
+		},
+		{
+			language: 'Japanese',
+			word: '乾杯 (Kanpai) ',
+		},
+		{
+			language: 'Irish',
+			word: 'Sláinte (slawn-cha)',
+		},
+		{
+			language: 'German',
+			word: 'Prost',
+		},
+		{
+			language: 'French',
+			word: 'Santé',
+		},
+		{
+			language: 'Danish',
+			word: 'Skål',
+		},
+		{
+			language: 'Dutch',
+			word: 'Proost',
+		},
+		{
+			language: 'English',
+			word: 'Cheers',
+		},
+		{
+			language: 'Greek',
+			word: 'Στην υγειά σας (Stin Eye-ee-yass-ooh)',
+		},
+		{
+			language: 'Hawaiin',
+			word: 'Kāmau',
+		},
+		{
+			language: 'Hebrew',
+			word: "L'chaim",
+		},
+		{
+			language: 'Mandarin',
+			word: '干杯 (gān bēi)',
+		},
+	];
+
+	console.log(data.length);
+
+	function rollDice(max) {
+		const result = Math.floor(Math.random() * max);
+		console.log(roll);
+		setRoll(result);
+	}
+
+	return (
+		<div className="App">
+			<NumberOfRolls numRolls={numRolls} />
+			<Button onClick={() => rollDice(data.length)}>
+				Roll the dice!
+			</Button>
+			<Text>Result: {roll}</Text>
+		</div>
+	);
 }
 
 export default App;
---
+++ client/src/components/NumberOfRolls.js
@@ -0,0 +1,7 @@
+import { Text } from '@chakra-ui/react';
+
+const NumberOfRolls = ({ numRolls }) => {
+	return <Text>Number of rolls: {numRolls}</Text>;
+};
+
+export default NumberOfRolls;
